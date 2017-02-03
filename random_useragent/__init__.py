0795a2208335a3a455a345ff94e89f7db537d21c
baffolobill <baffolobill@yandex.ru>
2017-02-03 23:04:46 +0300

    fixed wrong module import; prettify code structure

--- random_useragent/__init__.py
+++ random_useragent/__init__.py
@@ -1,48 +1,11 @@
-#!/usr/bin/python
-# -*-coding:utf-8-*-
-"""Scrapy Middleware to set a random User-Agent for every Request.
+from .random_useragent_middleware import RandomUserAgentMiddleware
 
-Downloader Middleware which uses a file containing a list of
-user-agents and sets a random one for each request.
-"""
-
-import random
-from scrapy import signals
-from scrapy.downloadermiddlewares.useragent import UserAgentMiddleware
-from . import default_user_agents
 
 __author__ = "Srinivasan Rangarajan"
 __copyright__ = "Copyright 2016, Srinivasan Rangarajan"
 __credits__ = ["Srinivasan Rangarajan", "Tianhui Michael Li"]
 __license__ = "MIT"
-__version__ = "0.2"
+__version__ = "0.2.2"
 __maintainer__ = "Srinivasan Rangarajan"
 __email__ = "srinivasanr@gmail.com"
 __status__ = "Development"
-
-
-class RandomUserAgentMiddleware(UserAgentMiddleware):
-
-    def __init__(self, settings, user_agent='Scrapy'):
-        super(RandomUserAgentMiddleware, self).__init__()
-        self.user_agent = user_agent
-        user_agent_list_file = settings.get('USER_AGENT_LIST')
-        if not user_agent_list_file:
-            # If USER_AGENT_LIST_FILE settings is not set,
-            # use the default ones provided
-            self.user_agent_list = default_user_agents.DEFAULT_USER_AGENTS
-        else:
-            with open(user_agent_list_file, 'r') as f:
-                self.user_agent_list = [line.strip() for line in f.readlines()]
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        obj = cls(crawler.settings)
-        crawler.signals.connect(obj.spider_opened,
-                                signal=signals.spider_opened)
-        return obj
-
-    def process_request(self, request, spider):
-        user_agent = random.choice(self.user_agent_list)
-        if user_agent:
-            request.headers.setdefault('User-Agent', user_agent)
